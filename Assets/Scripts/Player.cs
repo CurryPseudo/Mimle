66de599b9eba6089e77c458667727df58aaecce0
CurryPseudo <jesonxiake@126.com>
2025-01-18 23:59:34 +0800

    fix: cross target bubble logic

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -207,12 +207,7 @@ public class Player : MonoBehaviour
                 {
                     // Check drilling out
                     var translation = Velocity.normalized * drillOutCheckDistance;
-                    var blockHit = ClosetTranslationHit(translation, hit =>
-                    {
-                        var bubbleColor = GetBubbleColor(hit);
-                        if (bubbleColor == null) return true;
-                        return bubbleColor.color != floodingBubbleColor.color;
-                    });
+                    var blockHit = ClosetTranslationHit(translation, hit => !IsBubbleColorHit(hit, floodingBubbleColor));
                     if (blockHit != null)
                         translation = translation.normalized * math.max(blockHit.Value.distance - hitEpsilon, 0);
                     {
@@ -234,12 +229,8 @@ public class Player : MonoBehaviour
                 if (drillingOut)
                 {
                     var translation = Velocity * Time.fixedDeltaTime;
-                    var blockHit = ClosetTranslationHit(translation, hit =>
-                    {
-                        var bubbleColor = GetBubbleColor(hit);
-                        if (bubbleColor == null) return true;
-                        return bubbleColor.color != floodingBubbleColor.color;
-                    });
+                    var blockHit =
+                        ClosetTranslationHit(translation, hit => !IsBubbleColorHit(hit, floodingBubbleColor));
                     if (blockHit != null)
                         translation = math.max(blockHit.Value.distance - hitEpsilon, 0f) * Velocity.normalized;
                     Vector2? drillStickPosition = null;
@@ -247,11 +238,7 @@ public class Player : MonoBehaviour
                     {
                         Position += translation;
                         var bubbleHit = ClosetTranslationHit(-translation, hit =>
-                        {
-                            var bubbleColor = GetBubbleColor(hit);
-                            if (bubbleColor == null) return false;
-                            return bubbleColor.color == floodingBubbleColor.color;
-                        });
+                            IsBubbleColorHit(hit, floodingBubbleColor));
                         if (!IsOverlappingBubble() && bubbleHit != null)
                             drillStickPosition = Position + -translation.normalized *
                                 math.max(bubbleHit.Value.distance - hitEpsilon, 0f);
@@ -267,7 +254,7 @@ public class Player : MonoBehaviour
 
                 {
                     var closetHit = ClosetTranslationHit(hit =>
-                        IsNotBubbleHit(hit) || GetBubbleColor(hit).color != floodingBubbleColor.color);
+                        !IsBubbleColorHit(hit, floodingBubbleColor));
                     if (closetHit == null)
                     {
                         MoveByVelocity();
@@ -285,6 +272,14 @@ public class Player : MonoBehaviour
             default:
                 throw new ArgumentOutOfRangeException();
         }
+    }
+
+    private bool IsBubbleColorHit(RaycastHit2D hit, BubbleColor floodingBubbleColor)
+    {
+        if (IsTargetBubbleHit(hit)) return false;
+        var bubbleColor = GetBubbleColor(hit);
+        if (bubbleColor == null) return true;
+        return bubbleColor.color == floodingBubbleColor.color;
     }
 
     private bool IsOverlappingBubble()
